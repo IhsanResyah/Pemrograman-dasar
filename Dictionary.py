31f5f18271fafb19b4650847e4577e4ee0a3e23f
IhsanResyah <ihsanresyah@gmail.com>
2025-11-10 15:28:26 +0800

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+# NAMA :Muh ihsan resyah
+# NIM : D0425512
+# Dictionary
+
+mahasiswa = ["rian", "ammar", "dermawan", "aco", "kurni"]
+
+# Menampilkan semua nama
+print("Daftar Mahasiswa:", mahasiswa)
+
+# Mengakses 
+print("Mahasiswa pertama:", mahasiswa[0])
+
+# Menambah data 
+mahasiswa.append("alim")
+print("Setelah ditambah:", mahasiswa)
+
+# Menghapus 
+mahasiswa.remove("mamud")
+print("Setelah mamud dihapus:", mahasiswa)
